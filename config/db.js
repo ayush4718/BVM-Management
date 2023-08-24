2cb80b0c6d09ec5987851b02363ca7a234b11218
unknown <imran.bvminfotech@gmail.com>
2023-08-24 16:14:17 +0530

    add vercel.json

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const db = {}
-db.url = 'mongodb+srv://ayush:Ayush@cluster0.m5nfaus.mongodb.net/test'
+db.url = process.env.URL
 db.monggose = mongoose
 
 module.exports = db
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express();
 const bodyparser = require('body-parser');
-const port = 3001
+const port = process.env.PORT;
 const db = require('./config/db')
 const routs = require("./routes/index");
 app.use(bodyparser());
@@ -12,7 +12,9 @@ db.monggose.connect(db.url).then(()=>{
 .catch((error)=>{
     console.log("ErroR",error)
 })
-
+app.get("/", (req,res)=>{
+    res.send("you are in beck end")
+})
 app.use(routs)
 
 app.listen(port,()=>{
